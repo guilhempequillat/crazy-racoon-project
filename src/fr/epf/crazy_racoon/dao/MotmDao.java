243d274423846b2ece62384e09553a707079f77b
Guilhempequillat <pequillat.guilhem@gmail.com>
2017-09-16 17:41:34 +0200

    Supprimer les motm du même mois et du même user

--- src/fr/epf/crazy_racoon/dao/MotmDao.java
+++ src/fr/epf/crazy_racoon/dao/MotmDao.java
@@ -7,6 +7,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import fr.epf.crazy_racoon.model.Motm;
+import fr.epf.crazy_racoon.model.User;
 
 @Singleton
 public class MotmDao {
@@ -15,5 +16,21 @@ public class MotmDao {
 	
 	public void save(Motm motm) {
 		em.persist(motm);
+	}
+	
+	public Motm findOne(Long id) {
+		return em.find(Motm.class, id);
+	}
+	
+	public List<Motm> findAll() {
+		return em.createQuery("FROM Motm").getResultList();
+	}
+	
+	public List<Motm> fillAllByIdUser(Long id_user){
+		return em.createQuery("FROM Motm WHERE user_id="+id_user).getResultList();
+	}
+	
+	public void deleteMotmById(Long id) {
+		em.remove(findOne(id));
 	}
 }
--- src/fr/epf/crazy_racoon/servlet/MotmFormServlet.java
+++ src/fr/epf/crazy_racoon/servlet/MotmFormServlet.java
@@ -1,8 +1,11 @@
 package fr.epf.crazy_racoon.servlet;
 
+import java.util.List;
 import java.io.IOException;
 import java.sql.Date;
 import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Calendar;
 
 import javax.inject.Inject;
 import javax.servlet.ServletException;
@@ -47,6 +50,24 @@ public class MotmFormServlet extends HttpServlet {
 		Date motmDate=new Date(stamp.getTime());
 		int grade=Integer.parseInt(req.getParameter("grade"));
 		Motm motm=new Motm(grade,comment,user,motmDate);
+		Long testId=(long) 1;
+		checkMotmDate(testId);
 		return motm;
+	}
+	public void checkMotmDate(Long idUser) {
+		Timestamp stamp = new Timestamp(System.currentTimeMillis());
+		Date stampDate=new Date(stamp.getTime());
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTime(stampDate);
+		List<Motm> listMotm=new ArrayList<Motm>();
+		listMotm=motmDao.fillAllByIdUser(idUser);
+		for(int i=0;i<listMotm.size();i++) {
+			java.util.Date motmDate= listMotm.get(i).getMotmDate();
+			Calendar calendarMotm = Calendar.getInstance();
+			calendarMotm.setTime(motmDate);
+			if(calendarMotm.get(Calendar.MONTH) == calendar.get(Calendar.MONTH) && calendarMotm.get(Calendar.YEAR)== calendar.get(Calendar.YEAR)) {
+				motmDao.deleteMotmById(listMotm.get(i).getId());
+			}
+		}
 	}
 }
