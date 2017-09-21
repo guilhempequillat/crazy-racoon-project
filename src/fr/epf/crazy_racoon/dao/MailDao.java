cc7e4213981a3150329e475eb547bc1cbc661573
eteurnier <emilie.teurnier@epfedu.fr>
2017-09-21 22:40:47 +0200

    Encore des problèmes

--- src/fr/epf/crazy_racoon/dao/MailDao.java
+++ src/fr/epf/crazy_racoon/dao/MailDao.java
@@ -10,32 +10,31 @@ import fr.epf.crazy_racoon.model.Mail;
 
 @Singleton
 public class MailDao {
-	
+
 	@PersistenceContext
 	private EntityManager em;
-	
+
 	public void save(Mail m) {
 		em.persist(m);
 	}
-	
+
 	public Mail findOne(Long id) {
 		return em.find(Mail.class, id);
 	}
-	
+
 	public List<Mail> findAll() {
 		return em.createQuery("FROM Mail").getResultList();
 	}
-	
+
 	public void updateOne(String subject, String content, Long id) {
-		Mail mailEdit =em.find(Mail.class, id);
-		if(subject.equals(mailEdit.getSubject())==false){
+		Mail mailEdit = em.find(Mail.class, id);
+		if (subject.equals(mailEdit.getSubject()) == false) {
 			mailEdit.setSubject(subject);
 		}
-		if(content.equals(mailEdit.getContent())==false){
+		if (content.equals(mailEdit.getContent()) == false) {
 			mailEdit.setContent(content);
 		}
 
 		em.flush();
 	}
 }
-
